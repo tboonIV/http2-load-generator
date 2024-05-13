0ed436ee5a6279e3cc15c07b78332c770adc1d51
Jason Lee <lwlee2608@gmail.com>
2024-05-13 11:46:55 +0800

    handle scipritng context and variable assignment

--- src/scripting/mod.rs
+++ src/scripting/mod.rs
@@ -12,47 +12,15 @@ use std::error::Error;
 // def count = 0
 // def count = count + 1
 //
+//
 #[derive(Debug)]
-pub struct Scripting {
-    pub raw: String,
+pub struct Context {
     pub variables: Vec<Variable>,
 }
 
-impl Scripting {
-    pub fn new(raw: &str) -> Self {
-        Scripting {
-            raw: raw.into(),
-            variables: vec![],
-        }
-    }
-
-    pub fn eval(&mut self) -> Result<(), Box<dyn Error>> {
-        let lines: Vec<&str> = self.raw.split('\n').collect();
-        for line in lines {
-            let parts: Vec<&str> = line.split(' ').collect();
-            if parts.len() > 0 {
-                let def = parts[0];
-                if def == "def" {
-                    if parts.len() != 4 {
-                        return Err("Invalid script".into());
-                    }
-                    let name = parts[1];
-                    let equal = parts[2];
-                    if equal != "=" {
-                        return Err("Invalid script".into());
-                    }
-                    let value = parts[3];
-                    let variable = Variable {
-                        name: name.into(),
-                        value: Value::Int(value.parse().unwrap()),
-                    };
-                    self.variables.push(variable);
-                } else {
-                    return Err("Invalid script".into());
-                }
-            }
-        }
-        Ok(())
+impl Context {
+    pub fn new() -> Self {
+        Context { variables: vec![] }
     }
 
     pub fn get_variable(&self, name: &str) -> Option<Value> {
@@ -62,6 +30,71 @@ impl Scripting {
             }
         }
         None
+    }
+}
+
+#[derive(Debug)]
+pub struct Scripting {
+    pub raw: String,
+}
+
+impl Scripting {
+    pub fn new(raw: &str) -> Self {
+        Scripting { raw: raw.into() }
+    }
+
+    pub fn eval(&mut self, context: &mut Context) -> Result<(), Box<dyn Error>> {
+        let lines: Vec<&str> = self.raw.split('\n').collect();
+        for line in lines {
+            let parts: Vec<&str> = line.split(' ').collect();
+            if parts.len() > 0 {
+                let def = parts[0];
+                if def == "def" {
+                    // Validate
+                    if parts.len() < 4 {
+                        return Err(format!(
+                            "invalid script, expected at least 4 parts, got {}",
+                            parts.len()
+                        )
+                        .into());
+                    }
+                    // Process variable name
+                    let name = parts[1];
+
+                    // Process operator, only support '='
+                    let equal = parts[2];
+                    if equal != "=" {
+                        return Err("Invalid script, must be '='".into());
+                    }
+
+                    // Process value
+                    let value = parts[3];
+
+                    // Check if value is constant or variable
+                    let value = match value.parse::<i32>() {
+                        Ok(v) => Value::Int(v),
+                        Err(_) => {
+                            // Check if variable exists
+                            let variable = context.get_variable(value);
+                            if variable.is_none() {
+                                return Err(format!("variable '{}' not found", value).into());
+                            }
+                            variable.unwrap()
+                        }
+                    };
+
+                    let variable = Variable {
+                        name: name.into(),
+                        value,
+                    };
+
+                    context.variables.push(variable);
+                } else {
+                    return Err(format!("invalid script, unknown command '{}'", def).into());
+                }
+            }
+        }
+        Ok(())
     }
 }
 
@@ -84,9 +117,17 @@ mod tests {
 
     #[test]
     fn test_scripting() {
-        let mut scripting = Scripting::new("def count = 10");
-        scripting.eval().unwrap();
-        let count = scripting.get_variable("count").unwrap();
-        assert_eq!(count, Value::Int(10));
+        let mut context = Context::new();
+        let mut scripting = Scripting::new("def foo = 16");
+        scripting.eval(&mut context).unwrap();
+        let count = context.get_variable("foo").unwrap();
+        assert_eq!(count, Value::Int(16));
+
+        let mut scripting = Scripting::new("def count = foo");
+        scripting.eval(&mut context).unwrap();
+        let count = context.get_variable("count").unwrap();
+        assert_eq!(count, Value::Int(16));
+
+        // let mut scripting = Scripting::new("def count = count + 1");
     }
 }
